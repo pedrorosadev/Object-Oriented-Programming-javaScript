9b0aac2fe24b1d061821529fada7f5ecfe04ed80
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-09-19 12:49:46 -0300

    Atividades em desenvolvimento

--- atividade01/src/client.ts
+++ atividade01/src/client.ts
@@ -1,4 +1,20 @@
-class Client{
+interface newClientPF{
+    name:string;
+    gender:string;
+    birthDate:string;
+    cpf:string;
+    rg:string;
+    city:string;
+    uf:string;
+}
+interface newClientPJ{
+    companyName:string;
+    cnpj:string;
+    city:string;
+    uf:string;
+}
+
+export class Client{
     //Atributos da Classe Cliente referente para as Pessoas Físicas e Jurídicas
     public name:string;
     public companyName:string;
@@ -9,6 +25,24 @@ class Client{
     public cnpj:string;
     public city:string;
     public uf:string;
+
+    //Construtores
+    constructorPF({
+        name,
+        gender,
+        birthDate,
+        cpf,
+        rg,
+        city,
+        uf
+    }:newClientPF){}
+    
+    constructorPJ({
+        companyName,
+        cnpj,
+        city,
+        uf
+    }:newClientPJ){}
 
     //Métodos da Classe Cliente
 
---
+++ atividade02/src/script.ts
@@ -0,0 +1,5 @@
+import {Plane} from './plane';
+const prompt = require('prompt-sync');
+
+//const plane = new Plane();
+
